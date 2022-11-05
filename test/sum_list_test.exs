ede87285a334506a16892d20786797fb3a11f0ce
JeffLuis <jeruchiha1@gmail.com>
2022-11-05 01:34:03 -0300

    first commit

---
+++ test/sum_list_test.exs
@@ -0,0 +1,23 @@
+defmodule SumListTest do
+  use ExUnit.Case
+
+  describe "call/1" do
+    test "return the list sum" do
+      list = [1, 2, 3]
+
+      response = SumList.call(list)
+
+      expected_response = 6
+
+      assert response == expected_response
+    end
+  end
+end
+
+# defmodule SumListTest do
+# describe "call/1" do
+#   test "returns the list sum" do
+#     assert SumList.call([1,2,3]) == 6
+#   end
+# end
+# end
